3f8b5ac8909c4f1e0c4250671ce00bb2b8430eae
문승훈 <moonseunghun@munseunghun-ui-MacBookPro.local>
2023-01-20 14:46:49 +0900

    Add: 로그인

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp/SignUp";
 import Payment from "./pages/payment/Payment";
 import KakaoLogin from "./pages/SignUp/KakaoLogin";
 
@@ -8,9 +7,8 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
         <Route path="/payment" element={<Payment />} />
-        <Route path="/kakaologin" element={<KakaoLogin />} />
+        <Route path="/" element={<KakaoLogin />} />
       </Routes>
     </BrowserRouter>
   );
