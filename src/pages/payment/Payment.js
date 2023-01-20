29383d70566be3f6f67c7a4207aa84a2bb42ad05
minki3 <86992006+minki3@users.noreply.github.com>
2023-01-20 15:53:06 +0900

    api 연결 중

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -1,41 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import styled from "styled-components";
-
+import axios from "axios";
 const Payment = () => {
+  useEffect(() => {
+    axios(
+      `https://cors-anywhere.herokuapp.com/https://payment.yamuzin.net/payment/token`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+      }
+    ).then((result) => {
+      console.log(result);
+      if (result.accessToken) {
+        localStorage.setItem("token", result.accessToken);
+      }
+    });
+  }, []);
+
   const IMP = window.IMP;
-  IMP.init("imp07381648");
 
-  const payment = () => {
-    const data = {
-      pg: "kcp.INIBillTst", // PG사
-      pay_method: "card", // 결제수단
-      merchant_uid: `mid_${new Date().getTime()}`, // 주문번호
-      amount: 1000, // 결제금액
-      name: "아임포트 결제 데이터 분석", // 주문명
-      buyer_name: "홍길동", // 구매자 이름
-      buyer_tel: "01012341234", // 구매자 전화번호
-      buyer_email: "example@example", // 구매자 이메일
-      buyer_addr: "신사동 661-16", // 구매자 주소
-      buyer_postcode: "06018", // 구매자 우편번호
-    };
-
-    IMP.request_pay(data, callback);
-  };
-
-  const callback = (response) => {
-    const { success, merchant_uid, error_msg } = response;
-
-    if (success) {
-      alert("success!");
-    } else {
-      alert(`결제 실패 ${error_msg}`);
-    }
-  };
+  function iamport() {
+    //가맹점 식별코드
+    IMP.init("imp07381648");
+    IMP.request_pay(
+      {
+        pg: "html5_inicis",
+        pay_method: "card",
+        merchant_uid: "merchant_" + new Date().getTime(),
+        name: "상품1", //결제창에서 보여질 이름
+        amount: 100, //실제 결제되는 가격
+        buyer_email: "iamport@siot.do",
+        buyer_name: "구매자이름",
+        buyer_tel: "010-1234-5678",
+        buyer_addr: "서울 강남구 도곡동",
+        buyer_postcode: "123-456",
+      },
+      function (rsp) {
+        console.log(rsp);
+        if (rsp.success) {
+          var msg = "결제가 완료되었습니다.";
+          msg += "고유ID : " + rsp.imp_uid;
+          msg += "상점 거래ID : " + rsp.merchant_uid;
+          msg += "결제 금액 : " + rsp.paid_amount;
+          msg += "카드 승인번호 : " + rsp.apply_num;
+        } else {
+          var msg = "결제에 실패하였습니다.";
+          msg += "에러내용 : " + rsp.error_msg;
+        }
+        alert(msg);
+      }
+    );
+  }
   return (
     <Container>
       <Card>
         <div>사과</div>
-        <BuyButton onClick={payment}>구매하기</BuyButton>
+        <BuyButton onClick={iamport}>구매하기</BuyButton>
       </Card>
     </Container>
   );
